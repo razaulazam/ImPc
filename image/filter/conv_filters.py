ffd1977557daad3f5099c813e5bd769702b6e17a
Raza UlAzam <Raza.UlAzam@faro.com>
2022-07-25 09:00:59 +0200

    Added more filters

--- image/filter/conv_filters.py
+++ image/filter/conv_filters.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 
-from typing import Union, Optional
+from typing import Union, Optional, Tuple, List
 from collections import namedtuple
-from commons.exceptions import FilteringError, WrongArgumentsType
+from commons.exceptions import FilteringError, WrongArgumentsType, WrongArgumentsValue
+from commons.warning import DefaultSetting
 from image.load._interface import PyFaroImage
 from image._decorators import check_image_exist_external
 from image._helpers import image_array_check_conversion, check_user_provided_ndarray
+
+# -------------------------------------------------------------------------
 
 BORDER_INTERPOLATION = {
     "constant": cv2.BORDER_CONSTANT,
@@ -17,6 +20,8 @@ BORDER_INTERPOLATION = {
     "default": cv2.BORDER_DEFAULT
 }
 
+# -------------------------------------------------------------------------
+
 @check_image_exist_external
 def corr2d(
     image: PyFaroImage,
@@ -24,6 +29,8 @@ def corr2d(
     delta: Optional[Union[float, int]] = 0,
     border: Optional[str] = "default",
 ) -> PyFaroImage:
+    """Outputs the image with the same depth. Places the computed/filtered value in the center of the area covered by the kernel.
+       Note: Kernel windows are not variable sized here. They have a constant size over each pixel neighborhood."""
 
     image_array_check_conversion(image, "openCV")
 
@@ -45,7 +52,10 @@ def corr2d(
 
     border_actual = BORDER_INTERPOLATION.get(border, None)
     if not border_actual:
-        border_actual = cv2.BORDER_DEFAULT
+        DefaultSetting(
+            "Provided border option is not supported currently. Using the default strategy (reflect)"
+        )
+        border_actual = BORDER_INTERPOLATION["default"]
 
     try:
         new_im = image.copy()
@@ -57,7 +67,65 @@ def corr2d(
 
     return new_im
 
+# -------------------------------------------------------------------------
+
+def blur(
+    image: PyFaroImage,
+    kernel_size: Union[List[int], Tuple[int, int]],
+    normalize: Optional[bool] = True,
+    border: Optional[str] = "default"
+) -> PyFaroImage:
+    """Wrap border is not supported here. Normalize = False can be used to extract useful image chracteristics e.g. covariance matrix of the image gradients
+    can help with extracting images demonstrating optical flow for object tracking."""
+
+    image_array_check_conversion(image, "openCV")
+
+    if not isinstance(kernel_size, (Tuple, List)):
+        raise WrongArgumentsType("Kernel size can only be defined in form of a tuple or list")
+
+    if len(kernel_size) != 2:
+        raise WrongArgumentsValue("Kernal size can only span in the height and the width direction")
+
+    if not all(i > 0 for i in kernel_size):
+        raise WrongArgumentsValue("Kernel size cannot be negative")
+
+    if not isinstance(normalize, bool):
+        raise WrongArgumentsType("Normalize argument can only be specified as a boolean")
+
+    if not isinstance(border, str):
+        raise WrongArgumentsType("Border argument can only be specified as a string")
+
+    if border == "wrap":
+        DefaultSetting(
+            "Provided border option is not supported for this operation. Using the default strategy (reflect)"
+        )
+        border_actual = BORDER_INTERPOLATION["default"]
+    else:
+        border_actual = BORDER_INTERPOLATION.get(border, "None")
+
+    if not border_actual:
+        DefaultSetting(
+            "Provided border option is not supported by the library currently. Using the default strategy (reflect)"
+        )
+        border_actual = BORDER_INTERPOLATION["default"]
+
+    try:
+        new_im = image.copy()
+        new_im.image = cv2.boxFilter(
+            new_im.image, -1, kernel_size, normalize=normalize, borderType=border_actual
+        )
+        new_im.update_file_stream()
+        new_im.set_loader_properties()
+    except Exception as e:
+        raise FilteringError("Failed to filter the image") from e
+
+    return new_im
+
+# -------------------------------------------------------------------------
+
+
 if __name__ == "__main__":
     path_image = "C:\\dev\\pyfaro\\sample.jpg"
     a = cv2.imread(path_image)
+    b = cv2.blur(a, (1000, 1000))
     print("hallo")
