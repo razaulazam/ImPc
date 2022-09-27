70be2648263c71a29ffb941dd67859a502acbd17
razaulazam <razaulazam@outlook.com>
2022-09-28 01:59:38 +0500

    Implemented blobs determinant of hessian method

--- image/feature/methods.py
+++ image/feature/methods.py
@@ -10,6 +10,7 @@ from image._helpers import image_array_check_conversion
 from typing import Optional, Union
 from skimage.feature import canny as sk_canny
 from skimage.feature import blob_dog as sk_blob_dog
+from skimage.feature import blob_doh as sk_blob_doh
 
 # -------------------------------------------------------------------------
 
@@ -43,7 +44,7 @@ def canny(image: BaseImage, sigma: Optional[float] = 1.0, thresh_low: Optional[U
 # -------------------------------------------------------------------------
 
 @check_image_exist_external
-def blob_diff_gaussian(image: BaseImage, sigma_min: Optional[float] = 1.0, sigma_max: Optional[float] = 50.0, sigma_ratio: Optional[float] = 1.6, threshold: Optional[float] = 0.5, overlap: Optional[float] = 0.5):
+def blob_diff_gaussian(image: BaseImage, sigma_min: Optional[float] = 1.0, sigma_max: Optional[float] = 50.0, sigma_ratio: Optional[float] = 1.6, threshold: Optional[float] = 0.5, overlap: Optional[float] = 0.5) -> np.ndarray:
     """Compute blobs in a grayscale image using difference of gaussian method"""
 
     if not image.is_gray():
@@ -79,8 +80,8 @@ def blob_diff_gaussian(image: BaseImage, sigma_min: Optional[float] = 1.0, sigma
 # -------------------------------------------------------------------------
 
 @check_image_exist_external
-def blob_determinant_hessian(image: BaseImage, sigma_min: Optional[float] = 1.0, sigma_max: Optional[float] = 50.0, sigma_ratio: Optional[float] = 1.6, sigma_num: Optional[Union[int, float]] = 10, overlap: Optional[float] = 0.5):
-    """Compute blobs in a grayscale image using difference of gaussian method"""
+def blob_determinant_hessian(image: BaseImage, sigma_min: Optional[float] = 1.0, sigma_max: Optional[float] = 50.0, sigma_ratio: Optional[float] = 1.6, sigma_num: Optional[Union[int, float]] = 10, overlap: Optional[float] = 0.5) -> np.ndarray:
+    """Compute blobs in a grayscale image using determinant of hessian method"""
 
     if not image.is_gray():
         raise WrongArgumentsType("Input image should be grayscale for this method to work")
@@ -94,8 +95,8 @@ def blob_determinant_hessian(image: BaseImage, sigma_min: Optional[float] = 1.0,
     if not isinstance(sigma_ratio, float):
         raise WrongArgumentsType("Sigma ratio should be provided as float")
 
-    if not isinstance(threshold, float):
-        raise WrongArgumentsType("Threshold must be provided as float")
+    if not isinstance(sigma_num, (int, float)):
+        raise WrongArgumentsType("Number of sigmas must be provided as float or int")
 
     if not isinstance(overlap, float):
         raise WrongArgumentsType("Overlap must be provided as float")
@@ -106,13 +107,14 @@ def blob_determinant_hessian(image: BaseImage, sigma_min: Optional[float] = 1.0,
     check_image = image_array_check_conversion(image)
 
     try:
-        found_blobs = sk_blob_dog(check_image.image, sigma_min, sigma_max, sigma_ratio, threshold, overlap)
+        found_blobs = sk_blob_doh(check_image.image, sigma_min, sigma_max, sigma_ratio, int(sigma_num), overlap)
     except Exception as e:
         raise FeatureError("Failed to find the blobs in the image") from e
 
     return found_blobs
 
 # -------------------------------------------------------------------------
+
 if __name__ == "__main__":
     from pathlib import Path
     import numpy as np
