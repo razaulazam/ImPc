0446f7d05bcd83dd72229a72fe864622a08c9212
razaulazam <razaulazam@outlook.com>
2023-01-01 15:06:56 +0100

    Setup the correct package name

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup, find_packages
 
 # ------------------------------------------------------------------------
 
-setup(name="improcmagic", packages=find_packages())
+setup(name="impc", packages=find_packages())
