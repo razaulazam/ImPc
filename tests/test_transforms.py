1d7fd0ea062c7e8c6b03ff3ee296bc0ca9b5ca8b
razaulazam <razaulazam@outlook.com>
2023-03-21 00:28:58 +0000

    Finalized all the tests

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -16,7 +16,7 @@ def sample_data_path():
 
 # -------------------------------------------------------------------------
 
-def test_bilateral_filter(sample_data_path):
+def test_resize(sample_data_path):
     # Open the image.
     im = open_image(sample_data_path)
     _ = resize(im, (40, 50))
@@ -29,7 +29,7 @@ def test_bilateral_filter(sample_data_path):
 
 # -------------------------------------------------------------------------
 
-def test_bilateral_filter(sample_data_path):
+def test_rotate(sample_data_path):
     # Open the image.
     im = open_image(sample_data_path)
     _ = rotate(im, 10)
@@ -39,7 +39,39 @@ def test_bilateral_filter(sample_data_path):
         _ = rotate(im, angle="40")
 
 # -------------------------------------------------------------------------
-if __name__ == "__main__":
-    a = str(Path(__file__).parent / "data" / "sample.jpg")
-    b = open_image(a)
-    c = rotate(b, 10)
+
+def test_transpose(sample_data_path):
+    # Open the image.
+    im = open_image(sample_data_path)
+    _ = transpose(im)
+
+# -------------------------------------------------------------------------
+
+def test_kmeans_quantize(sample_data_path):
+    # Open the image.
+    im = open_image(sample_data_path)
+    _ = kmeans_quantize(im)
+
+    # Trigger the errors and warnings.
+    with pytest.raises(WrongArgumentsType):
+        _ = kmeans_quantize(im, clusters="40")
+
+# -------------------------------------------------------------------------
+
+def test_histogram_equalization(sample_data_path):
+    # Open the image.
+    im = open_image(sample_data_path)
+    _ = histogram_equalization(im)
+
+# -------------------------------------------------------------------------
+
+def test_convert_color(sample_data_path):
+    # Open the image.
+    im = open_image(sample_data_path)
+    _ = convert_color(im, "rgb2gray")
+
+    # Trigger the errors and warnings.
+    with pytest.raises(WrongArgumentsValue):
+        _ = convert_color(im, "rg2gr")
+
+# -------------------------------------------------------------------------
